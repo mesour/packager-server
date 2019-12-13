0974b7f09ad0752c676f9cf948a31c0ec38ab567
mesour <matous.nemec@gmail.com>
2019-12-13 16:57:58 +0100

    Fixed packager-server compression

--- bin/make-server.sh
+++ bin/make-server.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 rm ../generated/packager-server.compressed
 
 for source in $(find ../server/* -name '*' -type f); do
-    echo "${source/server\//} $(cat ${source} | base64)" >> ../generated/packager-server.compressed
+    echo "${source/..\/server\//} $(cat ${source} | base64)" >> ../generated/packager-server.compressed
 done
