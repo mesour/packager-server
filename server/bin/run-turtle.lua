29edb88eddd453e3a9e1022d7d73de4d8aeb3f48
mesour <matous.nemec@gmail.com>
2021-07-06 14:50:53 +0200

    Fixed startup for turtle

--- server/bin/run-turtle.lua
+++ server/bin/run-turtle.lua
@@ -1,4 +1,4 @@
-dofile("library/turtle/MiningTurtle.lua")
+dofile("library/turtle/Turtle.lua")
 dofile("library/turtle/TurtleHelper.lua")
 dofile("library/FileLoader.lua")
 dofile("library/RednetClient.lua")
@@ -7,12 +7,13 @@ local config = FileLoader.loadConfig("config.json")
 
 local rednetClient = RednetClient:create(config["rednet"])
 
-MiningTurtle.init(
+Turtle.init(
     config["name"],
     TurtleHelper.createVectorFromString(config["start"]),
     TurtleHelper.createVectorFromString(config["end"]),
     TurtleHelper.createVectorFromString(config["storage"]),
     TurtleHelper.createVectorFromString(config["torchStorage"]),
+    TurtleHelper.createVectorFromString(config["stoneStorage"]),
     config["storageSide"],
     rednetClient,
     {...}
